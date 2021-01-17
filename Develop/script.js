64e24b97f94ac4fefb36c5cbc8d5365725403572
Smack <kovacsmyles1@gmail.com>
2021-01-17 16:15:55 -0600

    fixed some phrasings

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,9 @@ var curentTime = moment();
 var tasks = {};
 let times = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#currentDay").text( moment().format('ddd MMM Do, YYYY') );
+    $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 
 });
 
@@ -24,34 +24,39 @@ $(".task").on("blur", "textarea", function () {
         .trim();
     //recreate p element
     var taskP = $("<p>")
-        .addClass("m-1")
+        .addClass("taskbox")
         .text(text);
 
     //replace textarea with p element
     $(this).replaceWith(taskP);
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+    saveTasks();
 });
+var saveTasks = function () {
+    localStorage.setItem("task", JSON.stringify(tasks))
+};
 
 $("saveBtn").on("click", "btn", function () {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-})
+    saveTasks();
+});
 
 var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks")); 
+    tasks = JSON.parse(localStorage.getItem("task"));
 }
 
-var styleTask = function (taskEl) {
+var styleRows = function (taskEl) {
     //get date from task element
-    var date = $(taskEl).find("time-block").text().trim();
+    var timeSlot = $(timesEl).find("time-block").text().trim();
 
-    //apply new class if task is near/over due date
-    if (moment().isAfter(currentTime)) {
+    //apply new class if slot is near/over due date
+    if (moment(timeSlot).isAfter(currentTime)) {
         $(taskEl).addClass("future");
     }
-    if (moment().isBefore(currentTime)) {
+    if (moment(timeSlot).isBefore(currentTime)) {
         $(taskEl).addClass("past");
     }
-    if (moment() === currentTime) {
+    if (moment(timeSlot) === currentTime) {
         $(taskEl).addClass("present");
     }
 };
+
+loadTasks();
